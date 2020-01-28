0e22c2358528d077d5b684b95f12e763733460e7
Dina Haner <dina.haner@consumer.org>
2020-01-27 19:26:42 -0500

    Changing the original R file to a markdown file

--- MakingdotRfile.R
+++ MakingdotRfile.R
@@ -1,3 +1,8 @@
+
+# '---
+# ' output: github_document
+# '---
+
 # Determine the bug when you run `get_climates()`
 # Hint: Use `traceback()` to find where it occurs, add breakpoints / `browser()` calls
 # Hint: look at types of input
